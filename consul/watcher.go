e22c92e2bb4887b82e2e8cefaa2cd7efe5ae4418
Sotirios Mantziaris <smantziaris@gmail.com>
2019-04-10 01:32:55 +0300

    handle multiple watches

--- consul/watcher.go
+++ consul/watcher.go
@@ -9,67 +9,146 @@ import (
 	"github.com/taxibeat/harvester"
 )
 
+// WatchItem definition.
+type WatchItem struct {
+	Type string
+	Key  string
+}
+
+// NewKeyWatchItem creates a new key watch item for the watcher.
+func NewKeyWatchItem(key string) WatchItem {
+	return WatchItem{Type: "key", Key: key}
+}
+
+// NewPrefixWatchItem creates a prefix key watch item for the watcher.
+func NewPrefixWatchItem(key string) WatchItem {
+	return WatchItem{Type: "prefix", Key: key}
+}
+
 // Watcher of Consul changes.
 type Watcher struct {
-	params              map[string]interface{}
+	datacenter          string
+	token               string
 	address             string
-	ch                  chan<- *harvester.Change
-	chErr               chan<- error
 	ignoreInitialChange bool
-	pl                  *watch.Plan
+	pp                  []*watch.Plan
 }
 
 // New creates a new watcher.
-func New(address string, params map[string]interface{}, ch chan<- *harvester.Change,
-	chErr chan<- error, ign bool) (*Watcher, error) {
+func New(address, datacenter, token string, ign bool) (*Watcher, error) {
 	if address == "" {
 		return nil, errors.New("address is empty")
 	}
-	if len(params) == 0 {
-		return nil, errors.New("params are empty")
-	}
-	if ch == nil {
-		return nil, errors.New("channel is nil")
-	}
-	if chErr == nil {
-		return nil, errors.New("error channel is nil")
-	}
-	return &Watcher{address: address, params: params, ch: ch, chErr: chErr, ignoreInitialChange: ign}, nil
+	return &Watcher{address: address, ignoreInitialChange: ign}, nil
 }
 
 // Watch the setup key and prefixes for changes.
-func (w *Watcher) Watch() error {
-	var err error
-	w.pl, err = watch.Parse(w.params)
-	if err != nil {
-		return err
+func (w *Watcher) Watch(ch chan<- *harvester.Change, chErr chan<- error, ww ...WatchItem) error {
+	if ch == nil {
+		return errors.New("channel is nil")
 	}
-	w.pl.Handler = func(idx uint64, data interface{}) {
+	if chErr == nil {
+		return errors.New("error channel is nil")
+	}
+
+	for _, wi := range ww {
+		var pl *watch.Plan
+		var err error
+		switch wi.Type {
+		case "key":
+			pl, err = w.runKeyWatcher(ch, chErr, wi.Key)
+		case "prefix":
+			pl, err = w.runPrefixWatcher(ch, chErr, wi.Key)
+		}
+		if err != nil {
+			return err
+		}
+		w.pp = append(w.pp, pl)
+	}
+	return nil
+}
+
+// Stop the watcher.
+func (w *Watcher) Stop() error {
+
+	return w.Stop()
+}
+
+func (w *Watcher) runKeyWatcher(ch chan<- *harvester.Change, chErr chan<- error, key string) (*watch.Plan, error) {
+	params := map[string]interface{}{}
+	params["datacenter"] = w.datacenter
+	params["token"] = w.token
+	params["key"] = key
+	params["type"] = "key"
+	pl, err := watch.Parse(params)
+	if err != nil {
+		return nil, err
+	}
+	pl.Handler = func(idx uint64, data interface{}) {
 		if w.ignoreInitialChange {
 			w.ignoreInitialChange = false
 			return
 		}
 		buf, err := json.MarshalIndent(data, "", "    ")
 		if err != nil {
-			w.chErr <- err
+			chErr <- err
 			return
 		}
 		mp := make(map[string]interface{}, 0)
 		err = json.Unmarshal(buf, &mp)
 		if err != nil {
-			w.chErr <- err
+			chErr <- err
 			return
 		}
-		w.ch <- &harvester.Change{
+		ch <- &harvester.Change{
 			Key:     mp["Key"].(string),
 			Value:   base64.StdEncoding.EncodeToString([]byte(mp["Value"].(string))),
 			Version: mp["ModifyIndex"].(int),
 		}
 	}
-	return w.pl.Run(w.address)
+	err = pl.Run(w.address)
+	if err != nil {
+		return nil, err
+	}
+	return pl, nil
 }
 
-// Stop the watcher.
-func (w *Watcher) Stop() error {
-	return w.Stop()
+func (w *Watcher) runPrefixWatcher(ch chan<- *harvester.Change, chErr chan<- error, key string) (*watch.Plan, error) {
+	params := map[string]interface{}{}
+	params["datacenter"] = w.datacenter
+	params["token"] = w.token
+	params["key"] = key
+	params["type"] = "prefix"
+	pl, err := watch.Parse(params)
+	if err != nil {
+		return nil, err
+	}
+	pl.Handler = func(idx uint64, data interface{}) {
+		if w.ignoreInitialChange {
+			w.ignoreInitialChange = false
+			return
+		}
+		buf, err := json.MarshalIndent(data, "", "    ")
+		if err != nil {
+			chErr <- err
+			return
+		}
+		//TODO: this might be a array...
+		mp := make(map[string]interface{}, 0)
+		err = json.Unmarshal(buf, &mp)
+		if err != nil {
+			chErr <- err
+			return
+		}
+		ch <- &harvester.Change{
+			Key:     mp["Key"].(string),
+			Value:   base64.StdEncoding.EncodeToString([]byte(mp["Value"].(string))),
+			Version: mp["ModifyIndex"].(int),
+		}
+	}
+	err = pl.Run(w.address)
+	if err != nil {
+		return nil, err
+	}
+	return pl, nil
 }
--- consul/watcher_test.go
+++ consul/watcher_test.go
@@ -4,15 +4,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/taxibeat/harvester"
 )
 
 func TestNew(t *testing.T) {
 	type args struct {
 		address string
-		params  map[string]interface{}
-		ch      chan<- *harvester.Change
-		chErr   chan<- error
 	}
 	tests := []struct {
 		name    string
@@ -23,9 +19,6 @@ func TestNew(t *testing.T) {
 			name: "success",
 			args: args{
 				address: "addr",
-				params:  map[string]interface{}{"key": "value"},
-				ch:      make(chan *harvester.Change),
-				chErr:   make(chan error),
 			},
 			wantErr: false,
 		},
@@ -33,46 +26,13 @@ func TestNew(t *testing.T) {
 			name: "missing address",
 			args: args{
 				address: "",
-				params:  map[string]interface{}{"key": "value"},
-				ch:      make(chan *harvester.Change),
-				chErr:   make(chan error),
-			},
-			wantErr: true,
-		},
-		{
-			name: "missing params",
-			args: args{
-				address: "addr",
-				params:  nil,
-				ch:      make(chan *harvester.Change),
-				chErr:   make(chan error),
-			},
-			wantErr: true,
-		},
-		{
-			name: "missing channel",
-			args: args{
-				address: "addr",
-				params:  map[string]interface{}{"key": "value"},
-				ch:      nil,
-				chErr:   make(chan error),
-			},
-			wantErr: true,
-		},
-		{
-			name: "missing error channel",
-			args: args{
-				address: "addr",
-				params:  map[string]interface{}{"key": "value"},
-				ch:      make(chan *harvester.Change),
-				chErr:   nil,
 			},
 			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := New(tt.args.address, tt.args.params, tt.args.ch, tt.args.chErr, false)
+			got, err := New(tt.args.address, "datacenter", "token", false)
 			if tt.wantErr {
 				assert.Error(t, err)
 				assert.Nil(t, got)
