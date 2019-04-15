ff62e197375bb50503f8b5c582640159edf3faa2
Sotirios Mantziaris <smantziaris@gmail.com>
2019-04-15 18:43:45 +0300

    Refactored packages

--- harvester.go
+++ harvester.go
@@ -52,6 +52,34 @@ func LogErrorf(format string, v ...interface{}) {
 	logErrorf(format, v...)
 }
 
+// Source definition.
+type Source string
+
+const (
+	// SourceSeed defines a seed value.
+	SourceSeed Source = "seed"
+	// SourceEnv defines a value from environment variables.
+	SourceEnv Source = "env"
+	// SourceConsul defines a value from consul.
+	SourceConsul Source = "consul"
+)
+
+// Change contains all the information that
+type Change struct {
+	Src     Source
+	Key     string
+	Value   string
+	Version uint64
+}
+
+// GetValueFunc function definition for getting a value for a key from a source.
+type GetValueFunc func(key string) (string, error)
+
+// Monitor defines a monitoring interface.
+type Monitor interface {
+	Monitor()
+}
+
 // Harvester interface.
 type Harvester interface {
 	Harvest(cfg interface{}) error
