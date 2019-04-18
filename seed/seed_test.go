540a226d04c6643d87d3bd43262839fc0fc4dcfc
Sotirios Mantziaris <smantziaris@gmail.com>
2019-04-18 10:12:20 +0300

    Added seed tests

--- seed/seed_test.go
+++ seed/seed_test.go
@@ -11,9 +11,16 @@ import (
 )
 
 func TestSeeder_Seed(t *testing.T) {
+	require.NoError(t, os.Setenv("ENV_XXX", "XXX"))
 	require.NoError(t, os.Setenv("ENV_AGE", "25"))
 	c := testConfig{}
 	goodCfg, err := config.New(&c)
+	require.NoError(t, err)
+	invalidIntCfg, err := config.New(&testInvalidInt{})
+	require.NoError(t, err)
+	invalidFloatCfg, err := config.New(&testInvalidFloat{})
+	require.NoError(t, err)
+	invalidBoolCfg, err := config.New(&testInvalidBool{})
 	require.NoError(t, err)
 	type fields struct {
 		consulGet GetValueFunc
@@ -30,6 +37,9 @@ func TestSeeder_Seed(t *testing.T) {
 		{name: "success", fields: fields{consulGet: testConsulGetSuccess}, args: args{cfg: goodCfg}, wantErr: false},
 		{name: "consul get nil", args: args{cfg: goodCfg}, wantErr: true},
 		{name: "consul get error", fields: fields{consulGet: testConsulGetError}, args: args{cfg: goodCfg}, wantErr: true},
+		{name: "invalid int", args: args{cfg: invalidIntCfg}, wantErr: true},
+		{name: "invalid float", args: args{cfg: invalidFloatCfg}, wantErr: true},
+		{name: "invalid bool", fields: fields{consulGet: testConsulGetSuccess}, args: args{cfg: invalidBoolCfg}, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -55,7 +65,22 @@ type testConfig struct {
 	HasJob  bool    `seed:"true" env:"ENV_HAS_JOB" consul:"/config/has-job"`
 }
 
+type testInvalidInt struct {
+	Age int64 `seed:"XXX"`
+}
+
+type testInvalidFloat struct {
+	Balance float64 `env:"ENV_XXX"`
+}
+
+type testInvalidBool struct {
+	HasJob bool `consul:"/config/XXX"`
+}
+
 func testConsulGetSuccess(key string) (string, error) {
+	if key == "/config/XXX" {
+		return "XXX", nil
+	}
 	return "true", nil
 }
 
