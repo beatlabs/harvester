98345465d335952975e9249da197d5f269533c17
Alex Demin <alex@demins.com>
2020-01-14 13:54:13 +0300

    add tests for types set string functions Signed-off-by: Alex Demin <alex@demins.com>

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -18,6 +18,13 @@ func TestBool(t *testing.T) {
 	assert.Equal(t, "true", b.String())
 }
 
+func TestBool_SetString(t *testing.T) {
+	var b Bool
+	assert.Error(t, b.SetString("wrong"))
+	assert.NoError(t, b.SetString("true"))
+	assert.True(t, b.Get())
+}
+
 func TestInt64(t *testing.T) {
 	var i Int64
 	ch := make(chan struct{})
@@ -28,6 +35,13 @@ func TestInt64(t *testing.T) {
 	<-ch
 	assert.Equal(t, int64(10), i.Get())
 	assert.Equal(t, "10", i.String())
+}
+
+func TestInt64_SetString(t *testing.T) {
+	var i Int64
+	assert.Error(t, i.SetString("wrong"))
+	assert.NoError(t, i.SetString("10"))
+	assert.Equal(t, int64(10), i.Get())
 }
 
 func TestFloat64(t *testing.T) {
@@ -42,6 +56,13 @@ func TestFloat64(t *testing.T) {
 	assert.Equal(t, "1.230000", f.String())
 }
 
+func TestFloat64_SetString(t *testing.T) {
+	var f Float64
+	assert.Error(t, f.SetString("wrong"))
+	assert.NoError(t, f.SetString("1.230000"))
+	assert.Equal(t, 1.23, f.Get())
+}
+
 func TestString(t *testing.T) {
 	var s String
 	ch := make(chan struct{})
@@ -54,6 +75,12 @@ func TestString(t *testing.T) {
 	assert.Equal(t, "Hello", s.String())
 }
 
+func TestString_SetString(t *testing.T) {
+	var s String
+	assert.NoError(t, s.SetString("foo"))
+	assert.Equal(t, "foo", s.Get())
+}
+
 func TestSecret(t *testing.T) {
 	var s Secret
 	ch := make(chan struct{})
@@ -64,4 +91,10 @@ func TestSecret(t *testing.T) {
 	<-ch
 	assert.Equal(t, "Hello", s.Get())
 	assert.Equal(t, "***", s.String())
+}
+
+func TestSecret_SetString(t *testing.T) {
+	var s Secret
+	assert.NoError(t, s.SetString("foo"))
+	assert.Equal(t, "foo", s.Get())
 }
