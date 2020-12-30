a77cafd6f12932aec4727ff46bcaa26940ece864
Sotirios Mantziaris <smantziaris@gmail.com>
2020-12-30 16:17:37 +0200

    Notification addition

---
+++ examples/06_notification/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/beatlabs/harvester"
+	"github.com/beatlabs/harvester/sync"
+)
+
+type config struct {
+	IndexName      sync.String `seed:"customers-v1"`
+	CacheRetention sync.Int64  `seed:"43200" env:"ENV_CACHE_RETENTION_SECONDS"`
+	LogLevel       sync.String `seed:"DEBUG" flag:"loglevel"`
+}
+
+func main() {
+	ctx, cnl := context.WithCancel(context.Background())
+	defer cnl()
+
+	err := os.Setenv("ENV_CACHE_RETENTION_SECONDS", "86400")
+	if err != nil {
+		log.Fatalf("failed to set env var: %v", err)
+	}
+
+	cfg := config{}
+
+	h, err := harvester.New(&cfg).Create()
+	if err != nil {
+		log.Fatalf("failed to create harvester: %v", err)
+	}
+
+	err = h.Harvest(ctx)
+	if err != nil {
+		log.Fatalf("failed to harvest configuration: %v", err)
+	}
+
+	log.Printf("Config : IndexName: %s, CacheRetention: %d, LogLevel: %s\n", cfg.IndexName.Get(), cfg.CacheRetention.Get(), cfg.LogLevel.Get())
+}
