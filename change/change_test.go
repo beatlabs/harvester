c58197fbdbe25e6fe04c95b98c27656a0407ec9c
rbroggi <ro_broggi@hotmail.com>
2022-01-22 17:19:41 +0100

    * benchmarking Change slice against change slice pointer

--- change/change_test.go
+++ change/change_test.go
@@ -14,3 +14,80 @@ func TestChange(t *testing.T) {
 	assert.Equal(t, "value", c.Value())
 	assert.Equal(t, uint64(1), c.Version())
 }
+
+func BenchmarkChangeValueSlice200Bytes(b *testing.B) {
+	ch := make(chan []Change)
+	benchChangeSliceAndChannelSend(200, ch, b)
+	close(ch)
+}
+func BenchmarkChangePointerSlice200Bytes(b *testing.B) {
+	ch := make(chan []*Change)
+	benchChangePointerSliceAndChannelSend(200, ch, b)
+	close(ch)
+}
+func BenchmarkChangeValueSlice1Kb(b *testing.B) {
+	ch := make(chan []Change)
+	benchChangeSliceAndChannelSend(1000, ch, b)
+	close(ch)
+}
+func BenchmarkChangePointerSlice1Kb(b *testing.B) {
+	ch := make(chan []*Change)
+	benchChangePointerSliceAndChannelSend(1000, ch, b)
+	close(ch)
+}
+func BenchmarkChangeValueSlice10Kb(b *testing.B) {
+	ch := make(chan []Change)
+	benchChangeSliceAndChannelSend(10000, ch, b)
+	close(ch)
+}
+func BenchmarkChangePointerSlice10Kb(b *testing.B) {
+	ch := make(chan []*Change)
+	benchChangePointerSliceAndChannelSend(10000, ch, b)
+	close(ch)
+}
+func BenchmarkChangeValueSlice100Kb(b *testing.B) {
+	ch := make(chan []Change)
+	benchChangeSliceAndChannelSend(100000, ch, b)
+	close(ch)
+}
+func BenchmarkChangePointerSlice100Kb(b *testing.B) {
+	ch := make(chan []*Change)
+	benchChangePointerSliceAndChannelSend(100000, ch, b)
+	close(ch)
+}
+
+func benchChangeSliceAndChannelSend(sizeInBytes uint32, c chan []Change, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		changeSlice := make([]Change, 100)
+		for i := 0; i < 100; i++ {
+			changeSlice[i] = Change{
+				src:     "",
+				key:     "key",
+				value:   string(make([]byte, sizeInBytes)),
+				version: 0,
+			}
+		}
+		go func() {
+			c <- changeSlice
+		}()
+		<-c
+	}
+}
+
+func benchChangePointerSliceAndChannelSend(sizeInBytes uint32, c chan []*Change, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		changeSlice := make([]*Change, 100)
+		for i := 0; i < 100; i++ {
+			changeSlice[i] = &Change{
+				src:     "",
+				key:     "key",
+				value:   string(make([]byte, sizeInBytes)),
+				version: 0,
+			}
+		}
+		go func() {
+			c <- changeSlice
+		}()
+		<-c
+	}
+}
