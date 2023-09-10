68308a0cf720b163a83f4eecd75f98f36537656f
Sotirios Mantziaris <smantziaris@gmail.com>
2023-09-10 17:42:36 +0300

    Simplify examples

--- examples/main.go
+++ examples/main.go
@@ -8,34 +8,69 @@ import (
 	"regexp"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/beatlabs/harvester"
+	harvesterconfig "github.com/beatlabs/harvester/config"
 	harvestersync "github.com/beatlabs/harvester/sync"
+	"github.com/go-redis/redis/v8"
 	"github.com/hashicorp/consul/api"
 )
 
+const (
+	consulAddress = "127.0.0.1:8500"
+	consulDC      = ""
+	consulToken   = ""
+)
+
 type config struct {
-	IndexName      harvestersync.String  `seed:"customers-v1"`
-	CacheRetention harvestersync.Int64   `seed:"43200" env:"ENV_CACHE_RETENTION_SECONDS"`
-	LogLevel       harvestersync.String  `seed:"DEBUG" flag:"loglevel"`
-	OpeningBalance harvestersync.Float64 `seed:"0.0" env:"ENV_CONSUL_VAR" consul:"harvester/example_02/openingbalance"`
-	AccessToken    harvestersync.Secret  `seed:"defaultaccesstoken" consul:"harvester/example_04/accesstoken"`
-	Email          Email                 `seed:"foo@example.com" env:"ENV_EMAIL"`
+	// IndexName demonstrates only seed.
+	IndexName harvestersync.String `seed:"customers-v1"`
+	// CacheRetention demonstrates seed and env var.
+	CacheRetention harvestersync.Int64 `seed:"43200" env:"ENV_CACHE_RETENTION_SECONDS"`
+	// LogLevel demonstrates seed and flag.
+	LogLevel harvestersync.String `seed:"DEBUG" flag:"loglevel"`
+	// OpeningBalance demonstrates seed, env var and redis.
+	OpeningBalance harvestersync.Float64 `seed:"0.0" redis:"opening-balance"`
+	// AccessToken demonstrates seed and consul for a secret.
+	AccessToken harvestersync.Secret `seed:"defaultaccesstoken" consul:"harvester/example/accesstoken"`
+	// Email demonstrates seed for a custom type.
+	Email Email `seed:"foo@example.com"`
+}
+
+func (c *config) String() string {
+	return fmt.Sprintf("config: IndexName: %s CacheRetention: %d LogLevel: %s OpeningBalance: %f AccessToken: %s Email: %s",
+		c.IndexName.Get(), c.CacheRetention.Get(), c.LogLevel.Get(), c.OpeningBalance.Get(), c.AccessToken.Get(),
+		c.Email.String())
 }
 
 func main() {
 	ctx, cnl := context.WithCancel(context.Background())
 	defer cnl()
 
-	setEnvVars()
-
-	seedConsulVars("currentaccesstoken")
+	setEnvVarCacheRetention()
+	seedConsulAccessToken("currentaccesstoken")
+	setRedisOpeningBalance(ctx, "1000")
 
 	cfg := config{}
 
+	chNotify := make(chan harvesterconfig.ChangeNotification)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		for change := range chNotify {
+			log.Printf("notification: " + change.String())
+		}
+		wg.Done()
+	}()
+
+	redisClient := createRedisClient()
+
 	h, err := harvester.New(&cfg).
-		WithConsulSeed("127.0.0.1:8500", "", "", 0).
-		Create()
+		WithConsulSeed(consulAddress, consulDC, consulToken, 0).WithConsulMonitor(consulAddress, consulDC, consulToken, 0).
+		WithRedisSeed(redisClient).WithRedisMonitor(redisClient, 200*time.Millisecond).
+		WithNotification(chNotify).Create()
 	if err != nil {
 		log.Fatalf("failed to create harvester: %v", err)
 	}
@@ -45,37 +80,46 @@ func main() {
 		log.Fatalf("failed to harvest configuration: %v", err)
 	}
 
-	log.Printf("Config: IndexName: %s, CacheRetention: %d, LogLevel: %s, OpeningBalance: %f\n", cfg.IndexName.Get(), cfg.CacheRetention.Get(), cfg.LogLevel.Get(), cfg.OpeningBalance.Get())
+	log.Println(cfg.String())
+
+	seedConsulAccessToken("newtaccesstoken")
+	setRedisOpeningBalance(ctx, "2000")
+
+	time.Sleep(1 * time.Second) // Wait for the data to be updated async...
+
+	log.Println(cfg.String())
 }
 
-func setEnvVars() {
+func setEnvVarCacheRetention() {
 	err := os.Setenv("ENV_CACHE_RETENTION_SECONDS", "86400")
 	if err != nil {
 		log.Fatalf("failed to set env var: %v", err)
 	}
-
-	err = os.Setenv("ENV_EMAIL", "bar@example.com")
-	if err != nil {
-		log.Fatalf("failed to set env var: %v", err)
-	}
 }
 
-func seedConsulVars(accessToken string) {
+func seedConsulAccessToken(accessToken string) {
 	cl, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
 		log.Fatalf("failed to create consul client: %v", err)
 	}
-	p := &api.KVPair{Key: "harvester/example_02/openingbalance", Value: []byte("100.0")}
-	_, err = cl.KV().Put(p, nil)
-	if err != nil {
-		log.Fatalf("failed to put key value pair to consul: %v", err)
-	}
 
-	p = &api.KVPair{Key: "harvester/example_04/accesstoken", Value: []byte(accessToken)}
+	p := &api.KVPair{Key: "harvester/example/accesstoken", Value: []byte(accessToken)}
 	_, err = cl.KV().Put(p, nil)
 	if err != nil {
 		log.Fatalf("failed to put key value pair to consul: %v", err)
 	}
+}
+
+func setRedisOpeningBalance(ctx context.Context, amount string) error {
+	_, err := createRedisClient().Set(ctx, "opening-balance", amount, 0).Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{})
 }
 
 // regex to validate an email value.
